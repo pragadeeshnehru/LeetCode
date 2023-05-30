d4ed2fe6d0837067cd342b228cf2b7bb6c674b55
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2023-05-30 17:37:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.4 MB (99.97%)

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        return helper(num,0);
+    }
+
+    public int helper(int num , int steps) {
+        if(num == 0) {
+            return steps;
+        }
+
+        if(num % 2 == 0) {
+            return helper(num/2 , steps+1);
+        }
+        return helper(num-1 , steps+1);
+    }
+}
