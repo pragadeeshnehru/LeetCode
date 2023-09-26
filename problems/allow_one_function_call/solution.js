46d741d9cd60efa2ed0a355be9a1b3caf0c63f0f
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2023-09-26 14:58:23 +0000

    Sync LeetCode submission - Allow One Function Call (javascript)

---
+++ problems/allow_one_function_call/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+	let wasCalled = false;
+	let result;
+	return function(...args){
+		if(!wasCalled) {
+			wasCalled = true;
+			return fn(...args);
+		} else {
+			return undefined;
+		}
+  }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
