420013b706b6ec288f14fa55f32bd54a604c47d8
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2023-09-28 16:15:30 +0000

    Sync LeetCode submission - Reverse Bits (c)

---
+++ problems/reverse_bits/solution.c
@@ -0,0 +1,12 @@
+uint32_t reverseBits(uint32_t n) {
+    uint32_t rev = 0;
+    for(int i=0;i<31;i++){
+      if(n&1){
+         rev+=1;
+      }
+      rev<<=1;
+      n=n>>1;
+   }
+   if(n&1)rev+=1;
+   return rev;
+}
