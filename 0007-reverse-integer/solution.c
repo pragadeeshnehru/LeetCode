a022b9f630e00d5fc12ea9f4bab4b071b890129c
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2023-09-28 16:01:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.3 MB (17.77%)

--- 0007-reverse-integer/solution.c
+++ 0007-reverse-integer/solution.c
@@ -1,11 +1,11 @@
 int reverse(int x){
-       int reverse = 0;
+    int reverse = 0;
     while (x != 0) 
      {
         int r = x%10;
         x = x / 10;
-        if (reverse > 2147483647/10 || (reverse == 2147483647/ 10 && r > 7)) return 0;
-        if(reverse < -2147483648/10 || (reverse == -2147483648/ 10 && r < -8)) return 0;
+        if (reverse > INT_MAX/10 || (reverse == INT_MAX/ 10 && r > 7)) return 0;
+        if(reverse < INT_MIN/10 || (reverse == INT_MIN/ 10 && r < -8)) return 0;
         reverse = reverse * 10 + r;
      }
   return reverse;
