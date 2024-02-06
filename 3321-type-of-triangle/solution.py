20266728a1e3c91ddc745fed95e9c5df803f1c58
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2024-02-06 14:40:19 +0000

    [LeetCode Sync] Runtime - 8 ms (95.87%), Memory - 11.7 MB (12.04%)

---
+++ 3321-type-of-triangle/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def triangleType(self, nums):
+        nums.sort()
+        if nums[0] + nums[1] <= nums[2]: return "none"
+        if nums[0] == nums[1] and nums[1] == nums[2] :
+            return 'equilateral'
+        if nums[0] == nums[1] or nums[1] == nums[2] : 
+           return 'isosceles'
+        return 'scalene'
+        
