67a17dd0a60628c97ec598092b7dc2802c794ee1
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2024-03-14 16:19:03 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 46.9 MB (98.61%)

---
+++ 0966-binary-subarrays-with-sum/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        //by finding number of sun=barrys with sum <= goal ans subtracting it with sum <=goal-1 we compute count count og sum == goal
+        return atMost(nums,goal)-atMost(nums, goal-1);
+    }
+
+    private int atMost(int[] nums,int goal){
+        int head, tail=0,sum=0,result=0;
+        for(head =0;head<nums.length;head++){
+            sum += nums[head];
+            while(sum > goal && tail <= head) {
+                sum-= nums[tail];
+                tail++;
+            }
+            // finding number of subarray of sum atmost equal to goal
+            result += head - tail +1;
+        }
+        return result;
+    }
+}
