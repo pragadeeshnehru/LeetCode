383579ba69fd2eafc6fdbb0288ec902488e28360
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2024-03-22 10:54:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (37.47%)

---
+++ 0206-reverse-linked-list/solution.java
@@ -0,0 +1,37 @@
+/*class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}*/
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nextNode = null;
+        while (curr != null) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+
