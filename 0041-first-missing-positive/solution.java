ce7013500e6e513dd67e45e89f75651377416e84
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2024-03-26 17:54:13 +0000

    [LeetCode Sync] Runtime - 1 ms (96.03%), Memory - 55 MB (58.69%)

--- 0041-first-missing-positive/solution.java
+++ 0041-first-missing-positive/solution.java
@@ -2,14 +2,11 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int i=0;
         int n = nums.length;
-        
-        //store positive integer n in n-1 index eg .. 1 in 0, 3 in 2
+
         while(i<n) {
-            // current index
             int curr = nums[i] - 1;
 
-            if(nums[i]>0 && nums[i]<n && nums[i] != nums[curr]){
-                //swap values
+            if(nums[i]>0 && nums[i] <n && nums[i]!=nums[curr]){
                 int temp = nums[i];
                 nums[i] = nums[curr];
                 nums[curr] = temp;
@@ -17,15 +14,12 @@ class Solution {
                 i++;
             }
         }
-        
-        // return the missing number
-        for(int j=0;j<n;j++){
-            if(nums[j] != j+1) {
+
+        for (int j=0;j<n;j++){
+            if(nums[j]!= j+1){
                 return j+1;
             }
         }
-
-        // return n+1 number if all +ve int from 1 to n are present  
         return n+1;
     }
 }
