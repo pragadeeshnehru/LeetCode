d9165d237ba8bc5decc1ce026af390df1853e965
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2024-04-07 16:51:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.9 MB (95.34%)

---
+++ problems/0678-valid-parenthesis-string/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int leftMin =0,leftMax =0;
+        for(char c : s.toCharArray()){
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')'){
+                leftMin--;
+                leftMax--;
+            } else if (c == '*') {
+                leftMin--;
+                leftMax++;
+            }
+
+            if (leftMax < 0 ) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+        return leftMin == 0;
+    }
+}
