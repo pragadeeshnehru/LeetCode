9457c5cc6a9a69b2cc70c6c1b27118dfc216c10a
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2024-08-02 15:03:04 +0000

    [LeetCode Sync] Runtime - 83 ms (42.98%), Memory - 57.3 MB (89.07%)

---
+++ problems/2255-minimum-swaps-to-group-all-1s-together-ii/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSwaps = function(nums) {
+    const n = nums.length;
+    const totalOnes = nums.reduce((sum,num) => sum +num, 0);
+
+    if(totalOnes === 0 || totalOnes === n) return 0;
+
+    let currentOnes = nums.slice(0,totalOnes).reduce((sum,num) => sum +num, 0);
+    let maxOnes = currentOnes;
+
+    for(let i = 0; i < n; i++){
+        currentOnes -= nums[i];
+        currentOnes += nums[(i+totalOnes) % n];
+        maxOnes = Math.max(maxOnes, currentOnes);
+    }
+
+    return totalOnes - maxOnes;
+};
