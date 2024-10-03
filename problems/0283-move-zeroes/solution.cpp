be578533c73c9f441c448efd95f2fde550bf33a1
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2024-10-03 12:10:50 +0000

    [LeetCode Sync] Runtime - 19 ms (9.59%), Memory - 21.9 MB (77.04%)

---
+++ problems/0283-move-zeroes/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZero = 0; 
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZero] = nums[i];
+                lastNonZero++;
+            }
+        }
+
+        for (int i = lastNonZero; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
+
