c2bfddb4b5d9c7172ab70339228252e672966515
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2025-01-09 13:39:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.3 MB (13.55%)

---
+++ problems/2292-counting-words-with-a-given-prefix/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for(int i=0; i< words.length; i++){
+            if(words[i].startsWith(pref)) count++;
+        }
+
+        return count;
+    }
+}
