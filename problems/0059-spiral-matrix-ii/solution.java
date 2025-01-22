37dea36fc99f35f412bd79b3f6737f883375f623
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2025-01-22 06:25:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (44.77%)

---
+++ problems/0059-spiral-matrix-ii/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int rowStart = 0;
+        int rowEnd = n - 1;
+        int colStart = 0;
+        int colEnd = n - 1;
+        int num = 1;
+
+        int matrix[][] = new int[n][n];
+
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i++) {
+                matrix[rowStart][i] = num++;
+            }
+            rowStart++;
+
+            for (int i = rowStart; i <= rowEnd; i++) {
+                matrix[i][colEnd] = num++;
+            }
+            colEnd--;
+
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    matrix[rowEnd][i] = num++;
+                }
+            }
+            rowEnd--;
+
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    matrix[i][colStart] = num++;
+                }
+            }
+            colStart++;
+        }
+
+        return matrix;
+    }
+}
