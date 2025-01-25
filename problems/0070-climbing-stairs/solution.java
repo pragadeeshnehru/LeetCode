055383051b585a1aa96c6c11036df4a62435516d
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2025-01-25 18:11:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (50.88%)

--- problems/0070-climbing-stairs/solution.java
+++ problems/0070-climbing-stairs/solution.java
@@ -1,17 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        Map<Integer, Integer> memo = new HashMap<>();
-        memo.put(1, 1);
-        memo.put(2, 2);
-        return recursion(n, memo);
-    }
-
-    private int recursion(int n, Map<Integer, Integer> memo) {
-        if (memo.containsKey(n)) {
-            return memo.get(n);
+        if (n == 0 || n == 1) {
+            return 1;
         }
 
-        memo.put(n, recursion(n - 1, memo) + recursion(n - 2, memo));
-        return memo.get(n);
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
