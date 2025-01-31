7d59103e052a32281340c7ecfb08d3832415c93d
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2025-01-31 04:13:07 +0000

    [LeetCode Sync] Runtime - 8 ms (72.63%), Memory - 60.2 MB (65.70%)

---
+++ problems/0221-maximal-square/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] dp = new int[rows][cols];
+
+        int side = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    side = Math.max(side, dp[i][j]);
+                }
+            }
+        }
+        return side * side;
+    }
+}
