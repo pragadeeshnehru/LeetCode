acae8696698d285f74797246474cd37085dbc55c
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2025-02-04 03:47:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (65.17%)

---
+++ problems/0074-search-a-2d-matrix/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m*n - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int row = mid/n;
+            int col = mid%n;
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) low = mid+1;
+            else high = mid-1;
+        }
+        return false;
+    }
+
+}
