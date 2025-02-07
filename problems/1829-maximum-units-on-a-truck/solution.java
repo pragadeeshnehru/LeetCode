4a5e95350df3b98471c13266d1ebd73588543edb
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2025-02-07 06:11:23 +0000

    [LeetCode Sync] Runtime - 9 ms (92.54%), Memory - 45.6 MB (9.49%)

---
+++ problems/1829-maximum-units-on-a-truck/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes ,(a,b) -> b[1] -a[1]);
+        int ans =0;
+        for(int[] box : boxTypes) {
+            int count =  Math.min(box[0] , truckSize);
+            ans += count * box[1];
+            truckSize -= count;
+            if(truckSize == 0) return ans;
+        }
+        return ans;
+    }
+}
