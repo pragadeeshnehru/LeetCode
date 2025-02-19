c663f2bbaf16e61fdbdc300705a6cf15e957adb7
Pragadeesh Nehru <116655427+pragadeeshnehru@users.noreply.github.com>
2025-02-19 06:22:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.9 MB (71.32%)

--- problems/0405-convert-a-number-to-hexadecimal/solution.java
+++ problems/0405-convert-a-number-to-hexadecimal/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public String toHex(int num) {
-        char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-
         if(num == 0) return "0";
+        char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
         StringBuilder result = new StringBuilder();
         int count = 0;
-        while(num != 0 && count<8){
+        while(num != 0 && count !=8){
             result.append(map[num & 15]);
             num = num >> 4;
             count++;
